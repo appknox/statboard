f66628ff3aa9acc999bb78996be91fbef0bcf489
dhilipsiva <dhilipsiva@gmail.com>
2017-03-18 02:34:40 +0530

    Add models

--- statboard/core/admin.py
+++ statboard/core/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from statboard.core.models import Metric
 
-# Register your models here.
+admin.site.register(Metric)
--- statboard/core/models.py
+++ statboard/core/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+from json import loads, dumps
+from django.db.models import Model, CharField, TextField
 
-# Create your models here.
+
+class Metric(Model):
+    name = CharField(max_length=50)
+    data = TextField()
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def data_dict(self):
+        """
+        docstring for json_data
+        """
+        return loads(self.data)
+
+    def set_data(self, data_dict):
+        """docstring for set_data"""
+        self.data = dumps(data_dict)
+        self.save()
