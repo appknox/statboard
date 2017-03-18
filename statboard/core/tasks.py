171fbcc1c7117e5f1fcdabeefbb3d8fcc2fd0f7e
dhilipsiva <dhilipsiva@gmail.com>
2017-03-18 18:16:37 +0530

    GitHub PRs

--- statboard/core/tasks.py
+++ statboard/core/tasks.py
@@ -12,11 +12,27 @@ Author: dhilipsiva <dhilipsiva@gmail.com>
 Date created: 2017-03-18
 """
 
+import os
+from github import Github
+from django.conf import settings
+from statboard.core.models import Metric
 from statboard.core.config import huey # NOQA
 from huey.contrib.djhuey import crontab, periodic_task
+
+g = Github(os.environ['STATBOARD_GITHUB_TOKEN'])
 
 
 @periodic_task(crontab(minute='*/1'))
 def fetch_metrics():
     # Code to get data
-    print('Every five minutes this will be printed by the consumer')
+    print("getting github")
+    u = g.get_user('appknox')
+    r = u.get_repo('mycroft')
+    prs = r.get_pulls()
+    pr_list = []
+    for pr in prs:
+        pr_list.append(pr.title)
+    m = Metric.objects.get(name=settings.VIEW_GITHUB_PRS)
+    data = {'pr_list': pr_list}
+    m.set_data(data)
+    print("fetched github")
--- statboard/core/views.py
+++ statboard/core/views.py
@@ -7,4 +7,5 @@ from statboard.core.models import Metric
 def github_prs(request):
     metric = Metric.objects.get(name=settings.VIEW_GITHUB_PRS)
     return render(
-        request, '%s.html' % settings.VIEW_GITHUB_PRS, {'metric': metric})
+        request, '%s.html' % settings.VIEW_GITHUB_PRS,
+        {'pr_list': metric.data_dict['pr_list']})
